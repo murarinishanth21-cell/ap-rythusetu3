aae7831abcf84cd02b630282f0101d0e1df621f6
murari nishanth <muraeinishanth@muraris-MacBook-Neo.local>
2026-09-09 18:40:02 +0530

    Optimize Vite chunk splitting and chunkSizeWarningLimit

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,16 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
